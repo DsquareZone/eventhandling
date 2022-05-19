63691b6f9873917c48c8fff2b9757b2f65573b2f
Surya Prakash <97003032+DsquareZone@users.noreply.github.com>
2022-05-19 15:07:58 +0530

    onSubmit is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
+import React, { useState } from "react";
 import "./styles.css";
 
-export default function App() {
+const App = () => {
+  const [data, setData] = useState({
+    email: "",
+    password: ""
+  });
+  const changeHandler = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
   return (
     <div className="App">
-      <h1>Test</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <center>
+        <form>
+          <label> Email </label>
+          <input
+            type="email"
+            name="email"
+            onChange={changeHandler}
+          /> <br /> <br />
+          <label> Password </label>
+          <input
+            type="password"
+            name="password"
+            onChange={changeHandler}
+          />{" "}
+          <br /> <br />
+          <input type="submit" value="Login" onSubmit={submitHandler} />
+        </form>
+      </center>
     </div>
   );
-}
+};
+
+export default App;
